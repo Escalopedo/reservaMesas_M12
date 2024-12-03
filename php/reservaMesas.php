46998d602f8a4a0a9a8d1c25433e23a3cd91c7b2
PC-ERIC <eric.alcazarc@gmail.com>
2024-12-03 17:14:10 +0100

    reservaMesas.php en PDO

--- php/reservaMesas.php
+++ php/reservaMesas.php
@@ -10,28 +10,36 @@ if (empty($_SESSION['user_id'])) {
     exit();
 }
 
-require '../php/conexion.php'; 
+require '../php/conexion.php';
 
 if (isset($_GET['id'])) {
     $id_mesa = htmlspecialchars($_GET['id']);
-    $user_id =$_SESSION['user_id'];
+    $user_id = $_SESSION['user_id'];
 
-    $update_query_ocu = "
-        UPDATE tbl_ocupacion
-        SET estado_ocupacion = 'Ocupado', fecha_inicio = CURRENT_TIMESTAMP, id_camarero = ?
-        WHERE id_mesa = ? AND estado_ocupacion = 'Disponible';
-    ";
+    try {
+        // Preparar la consulta para actualizar el estado de la mesa
+        $update_query_ocu = "
+            UPDATE tbl_ocupacion
+            SET estado_ocupacion = 'Ocupado', fecha_inicio = CURRENT_TIMESTAMP, id_camarero = :id_camarero
+            WHERE id_mesa = :id_mesa AND estado_ocupacion = 'Disponible';
+        ";
 
+        $stmt = $conn->prepare($update_query_ocu);
 
-    $stmt_update_query_ocu = mysqli_prepare($conn, $update_query_ocu);
-    mysqli_stmt_bind_param($stmt_update_query_ocu, "ii", $user_id, $id_mesa);
-    mysqli_stmt_execute($stmt_update_query_ocu);
-    mysqli_stmt_close($stmt_update_query_ocu);
+        // Enlazar los parámetros
+        $stmt->bindParam(':id_camarero', $user_id, PDO::PARAM_INT);
+        $stmt->bindParam(':id_mesa', $id_mesa, PDO::PARAM_INT);
 
-    // Redirigir al final
-    header("Location: ../view/mesas.php");
+        // Ejecutar la consulta
+        $stmt->execute();
+
+        // Redirigir al final
+        header("Location: ../view/mesas.php");
+        exit();
+    } catch (PDOException $e) {
+        echo "Error al reservar la mesa: " . $e->getMessage();
+    }
 } else {
     echo "ID de mesa no proporcionado.";
 }
-
 ?>
