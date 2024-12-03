859cd3f5320180ea8aff9aa31bf7e62546779384
PC-ERIC <eric.alcazarc@gmail.com>
2024-12-03 17:06:56 +0100

    estadoMesaRecuperar.php a PDO

--- php/estadoMesaRecuperar.php
+++ php/estadoMesaRecuperar.php
@@ -1,29 +1,28 @@
 <?php
-require '../php/conexion.php'; 
+require '../php/conexion.php';
+
 try {
+    // Preparar la consulta
     $sqlRecuperarEstados = "SELECT id_mesa, estado_ocupacion FROM tbl_ocupacion WHERE estado_ocupacion NOT LIKE 'Registrada';";
+    $stmt = $conn->prepare($sqlRecuperarEstados);
 
-    $stmt_table_state = mysqli_stmt_init($conn);
-    mysqli_stmt_prepare($stmt_table_state, $sqlRecuperarEstados);
-    mysqli_stmt_execute($stmt_table_state);
-    $resultado = mysqli_stmt_get_result($stmt_table_state);
+    // Ejecutar la consulta
+    $stmt->execute();
 
-    $rows = mysqli_num_rows($resultado);
-    
-    if($rows === 0) {
+    // Obtener los resultados
+    $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (empty($resultado)) {
         echo "No se ha encontrado ninguna mesa.";
     } else {
         $ARRAYocupaciones = array();
-        while ($fila = mysqli_fetch_assoc($resultado)) {
+        foreach ($resultado as $fila) {
             $ARRAYocupaciones[$fila['id_mesa']] = $fila['estado_ocupacion'];
         }
+        // Guardar el resultado en la sesión
         $_SESSION['ARRAYocupaciones'] = $ARRAYocupaciones;
     }
-    mysqli_stmt_close($stmt_table_state);
-
-    
 } catch (Exception $e) {
-    echo "Error al encontrar ususario: " . $e->getMessage();
+    echo "Error al encontrar mesas: " . $e->getMessage();
 }
-
 ?>
