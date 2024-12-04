713f4e97d1e3a25629c3d5a0645f66b6fd7a8ecf
PC-ERIC <eric.alcazarc@gmail.com>
2024-12-04 17:34:23 +0100

    Modificación final de la base de datos

--- sql/restaurante_bbdd.sql
+++ sql/restaurante_bbdd.sql
@@ -1,14 +1,14 @@
--- Creación de la base de datos
+-- Crear la base de datos
 CREATE DATABASE restaurante_bbdd;
 USE restaurante_bbdd;
 
--- Creación de la tabla de roles
+-- Tabla de roles
 CREATE TABLE tbl_roles (
     id_rol INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nombre_rol VARCHAR(50) NOT NULL UNIQUE
 );
 
--- Creación de la tabla de usuarios
+-- Tabla de usuarios
 CREATE TABLE tbl_usuarios (
     id_usuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nombre_usuario VARCHAR(50) NOT NULL,
@@ -18,30 +18,41 @@ CREATE TABLE tbl_usuarios (
     id_rol INT NOT NULL
 );
 
--- Creación de la tabla de salas
-CREATE TABLE tbl_sala ( 
+-- Tabla de salas
+CREATE TABLE tbl_sala (
     id_sala INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     ubicacion_sala VARCHAR(25) NOT NULL
 );
 
--- Creación de la tabla de mesas
+-- Tabla de mesas
 CREATE TABLE tbl_mesa (
     id_mesa INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_sala INT NOT NULL,
     numero_sillas_mesa INT NOT NULL
 );
 
--- Creación de la tabla de ocupaciones
+-- Tabla de horarios
+CREATE TABLE tbl_horarios (
+    id_horario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    hora_inicio TIME NOT NULL,
+    hora_fin TIME NOT NULL,
+    UNIQUE (hora_inicio, hora_fin) -- Evitar franjas duplicadas
+);
+
+-- Tabla de ocupaciones
 CREATE TABLE tbl_ocupacion (
     id_ocupacion INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_mesa INT NOT NULL,
     id_usuario INT NULL,
-    fecha_inicio DATETIME,
-    fecha_final DATETIME,
-    estado_ocupacion VARCHAR(25)
+    fecha_inicio DATETIME NULL,
+    fecha_final DATETIME NULL,
+    fecha_reserva DATE NULL, -- Fecha de la reserva
+    id_horario INT NULL, -- Franja horaria de la reserva
+    tipo_ocupacion ENUM('Actual', 'Reserva') NOT NULL DEFAULT 'Actual',
+    estado_ocupacion VARCHAR(25) NOT NULL
 );
 
--- Añadir las constraints después de la creación de las tablas
+-- Declarar las FOREIGN KEYS por separado
 
 -- Relación entre tbl_usuarios y tbl_roles
 ALTER TABLE tbl_usuarios
@@ -62,3 +73,8 @@ FOREIGN KEY (id_mesa) REFERENCES tbl_mesa (id_mesa);
 ALTER TABLE tbl_ocupacion
 ADD CONSTRAINT FK_ocupacion_usuario
 FOREIGN KEY (id_usuario) REFERENCES tbl_usuarios (id_usuario);
+
+-- Relación entre tbl_ocupacion y tbl_horarios
+ALTER TABLE tbl_ocupacion
+ADD CONSTRAINT FK_ocupacion_horario
+FOREIGN KEY (id_horario) REFERENCES tbl_horarios (id_horario);
