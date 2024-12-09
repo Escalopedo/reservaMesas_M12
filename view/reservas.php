b42ae428c0ffdfc3b8fb78745ccbd254c3a276f1
PC-ERIC <eric.alcazarc@gmail.com>
2024-12-09 16:19:39 +0100

    Header reserva

--- view/reservas.php
+++ view/reservas.php
@@ -1,1 +1,63 @@
-CHRISTIAN GAY
+<?php
+
+// Iniciamos la sesión
+session_start();
+
+// Verificamos si la sesión del camarero está activa
+if (empty($_SESSION['user_id'])) {
+    // Si no está activo, redirigimos a la página de inicio de sesión
+    header("Location: ./index.php");
+    exit();
+}
+
+// Incluimos el archivo de conexión a la base de datos
+require '../php/conexion.php';
+require '../php/estadoMesaRecuperar.php';
+require_once '../php/functions.php';
+
+// Obtenemos el ID del camarero desde la sesión
+$id_camarero = $_SESSION['user_id'];
+
+// Función para obtener información del camarero
+$info_waiter = get_info_waiter_bbdd($conn, $id_camarero);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mesas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/mesas.css">
+</head>
+<body>
+    <!-- Cabecera -->
+        <header id="container_header">
+            <!-- Contenedor del usuario -->
+            <div id="container-username">
+                <!-- icono del usuario  -->
+                <div id="icon_profile_header">
+                    <img src="../img/logoSinFondo.png" alt="" id="icon_profile">
+                </div>
+                <!-- Contenedor de la información del usuario -->
+                <div id="username_profile_header">
+                    <p id="p_username_profile"><?php echo htmlspecialchars($info_waiter['username']) ?></p>
+                    <span class="span_subtitle"><?php echo htmlspecialchars($info_waiter['name']) . " " . htmlspecialchars($info_waiter['surname']) ?></span>
+                </div>
+            </div>
+
+            <!-- Contenedor del título de la página -->
+            <div id="container_title_header">
+                <h1 id="title_header"><strong>Dinner At Westfield</strong></h1>
+                <span class="span_subtitle">Gestión de mesas</span>
+            </div>
+
+            <!-- Contenedor de navegación -->
+            <nav id="nav_header">
+                <a href="./historico.php" class="btn btn-danger me-2 btn_custom_logOut">Histórico</a>
+                <a href="./mesas.php" class="btn btn-danger me-2 btn_custom_logOut">Mesas</a>
+                <a href="../php/cerrarSesion.php" class="btn btn-danger btn_custom_logOut m-1">Cerrar sesión</a>
+            </nav>
+        </header>
